e0d6c159aadda762d2824e6d5815543577df0cb9
Anas Zaami <zaamianas@outlook.com>
2026-09-16 19:12:32 +0100

    Add Header component and enable Tailwind CSS

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+function Header() {
+    return (
+        <header className ='flex'>
+            <div className='left'>
+                
+            </div>
+            <div className='center'>
+
+            </div>
+            <div className='right'>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
