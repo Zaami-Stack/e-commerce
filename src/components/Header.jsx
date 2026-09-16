d34464cc2ab60e623e8342bae28741958abcc37c
Anas Zaami <zaamianas@outlook.com>
2026-09-16 21:10:30 +0100

    adding cart icon and cart counter UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import "./Header.css";
 import userDefaultPicture from "../assets/icons/user-icon.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
@@ -10,7 +12,7 @@ function Header() {
     >
       <div className="left flex items-center flex-1">
         <h1 className="font-sans text-[20px] font-medium w-5 text-[#1D1D1F]">
-          <span className="text-blue-600">i</span>Planet
+          <span className="text-blue-800">i</span>Planet
         </h1>
       </div>
       <div className="center flex items-center flex-1">
@@ -21,14 +23,24 @@ function Header() {
           placeholder="Find your favorite products..."
         />
       </div>
-      <div className="flex justify-end items-center right flex-1  ">
-        <button>
+      <div className="flex gap-4 justify-end items-center right flex-1  ">
+        <button className="relative flex items-center">
+          <FontAwesomeIcon
+            icon={faCartShopping}
+            className="text-xl text-gray-700"
+          />
+          <span 
+          className="absolute -top-2 text-white -right-2 bg-blue-400/60 w-5 rounded-[50%]
+             font-normal text-[14px] text-center">5</span>
+        </button>
+        <button className="flex items-center">
           <img
             className="h-7 rounded-[50%]"
             src={userDefaultPicture}
             alt="user-image"
           />
         </button>
+        
       </div>
     </header>
   );
